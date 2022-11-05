504ed2a79b7d71de7218ed9f00d408d599ca77a5
白晗 <baih_1989@163.com>
2022-11-05 18:10:50 +0800

    脚手架搭建项目

---
+++ 02_Vue组件化/01_vue_component/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2 class="title">{{ message }}</h2>
+  <home></home>
+</template>
+
+<script>
+import Home from './Home.vue'
+export default {
+  data() {
+    return {
+      message: "我是App组件"
+    }
+  },
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+.title {
+  color: red;
+}
+</style>
---
+++ 02_Vue组件化/01_vue_component/src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>我是Home组件</h2>
+    <p>我是一段段落</p>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style>
+
+</style>
