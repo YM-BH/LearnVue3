4dd2b6d0887ad5f4c77717cd08e93bc5d3e3ea25
白晗 <baih_1989@163.com>
2022-11-07 16:02:47 +0800

    组件通信 父传子

---
+++ 02_Vue组件化/01_vue_component/src/01_组件通信-父传子/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <show-info name="James" :age="38" :height="2.03" :info="{name: '巴拉'}" :level="2"></show-info>
+  <show-info :level="34"></show-info>
+</template>
+
+<script>
+import ShowInfo from './ShowInfo.vue'
+export default {
+  
+  components: {
+    ShowInfo
+  }
+
+}
+</script>
+
+<style scoped>
+</style>
---
+++ 02_Vue组件化/01_vue_component/src/01_组件通信-父传子/ShowInfo.vue
@@ -0,0 +1,41 @@
+<template>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>身高：{{ height }}</h2>
+  <h3>级别：{{ level }}</h3>
+  <span>{{info.name}}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "Curry"
+    },
+    age: {
+      type: Number,
+      default: 33
+    },
+    height: {
+      type: Number,
+      default: 1.91
+    },
+    level: {
+      type: Number,
+      required: true
+    },
+    // 属性是对象类型，default是函数，并返回一个对象
+    info: {
+      type: Object,
+      default() {
+        return {name: "属性是对象类型的返回"}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
