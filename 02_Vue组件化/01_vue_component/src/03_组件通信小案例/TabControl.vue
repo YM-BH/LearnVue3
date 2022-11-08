389a67efa5962ad59c6f53b2ce693c7402b54648
白晗 <baih_1989@163.com>
2022-11-08 15:55:35 +0800

    组件通信小案例

---
+++ 02_Vue组件化/01_vue_component/src/03_组件通信小案例/TabControl.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-control">
+    <div 
+      class="tab-control-item"
+      :class="{active: index === currentIndex}"
+      v-for="(title, index) in titles"
+      @click="titleClick(index)"
+
+    >
+      <span>{{ title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShowInfo from '@/01_组件通信-父传子/ShowInfo.vue';
+
+  export default {
+    props: {
+      titles: {
+        type: Array
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    emits: ['itemClick'],
+    methods: {
+      titleClick(index) {
+        this.currentIndex = index
+        this.$emit('itemClick', index)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    height: 44px;
+    line-height: 44px;
+    font-weight: 800;
+    text-align: center;
+  }
+  .tab-control-item {
+    flex: 1;
+  }
+
+  .tab-control-item.active {
+    color: red;
+  }
+
+  .tab-control-item.active span {
+    padding: 8px;
+    border-bottom: solid red 2px;
+  }
+</style>
--- 02_Vue组件化/01_vue_component/src/main.js
+++ 02_Vue组件化/01_vue_component/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 // import App from './01_组件通信-父传子/App.vue'
 
-import App from './02_组件通信-子传父/App.vue'
+// import App from './02_组件通信-子传父/App.vue'
+
+import App from './03_组件通信小案例/App.vue'
 
 createApp(App).mount('#app')
