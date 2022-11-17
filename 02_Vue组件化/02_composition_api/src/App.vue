0882c6a9d54b3fae1d5252e28c61a01b393cd71c
白晗 <baih_1989@163.com>
2022-11-17 21:33:50 +0800

    reactive

--- 02_Vue组件化/02_composition_api/src/App.vue
+++ 02_Vue组件化/02_composition_api/src/App.vue
@@ -3,20 +3,35 @@
     <h2>当前计数：{{ counter }}</h2>
     <button @click="increment">+1</button>
     <button @click="decrement">-1</button>
+    <hr>
+    <h2>message: {{ account.username }}</h2>
+    <button @click="changeMessage">修改信息</button>
   </div>
 </template>
 
 <script>
 import useCounter from './hooks/useCounter'
+import { reactive } from 'vue'
 export default {
   setup() {
+
+    const account = reactive({
+      username: "Curry",
+      password: "1234"
+    })
+
+    const changeMessage = () => {
+      account.username = "LBJ"
+    }
 
     const { counter, increment, decrement} = useCounter()
     return {
       // ...useCounter()
       counter,
       increment,
-      decrement
+      decrement,
+      account,
+      changeMessage
     }
   }
 }
