0da66f2ce5637a0740bbb0bd21a145701b7ca7b9
白晗 <baih_1989@163.com>
2022-11-17 08:11:44 +0800

    Composition API

---
+++ 02_Vue组件化/02_composition_api/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>当前计数：{{ counter }}</h2>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+import useCounter from './hooks/useCounter'
+export default {
+  setup() {
+
+    const { counter, increment, decrement} = useCounter()
+    return {
+      // ...useCounter()
+      counter,
+      increment,
+      decrement
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
