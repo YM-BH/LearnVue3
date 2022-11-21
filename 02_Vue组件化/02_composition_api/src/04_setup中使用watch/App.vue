3e2fa45b946c6765305caa2e4918f0638298f24a
白晗 <baih_1989@163.com>
2022-11-21 08:49:36 +0800

    setup中watch的使用

---
+++ 02_Vue组件化/02_composition_api/src/04_setup中使用watch/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2>{{ message }}</h2>
+  <button @click="message = 'Hello React'">修改消息</button>
+  <h2>{{ info.name }} - {{ info.age }}</h2>
+  <button @click="info.friends.name = 'james'">修改消息</button>
+</template>
+
+<script>
+import { reactive, ref, watch } from 'vue';
+
+  export default {
+    setup() {
+      const message = ref("Hello Vue")
+
+      const info = reactive({
+        name: "steve",
+        age: 30,
+        friends: {
+          name: "kobe"
+        }
+      })
+
+      // 监听 message 修改
+      watch(message, (newValue, oldValue) => {
+        console.log(newValue, oldValue)
+      })
+
+      watch(() => ({...info}), (newValue, oldValue) => {
+        console.log(newValue, oldValue)
+      }, {
+        immediate: true,
+        deep: true
+      })
+
+      return {
+        message,
+        info
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- 02_Vue组件化/02_composition_api/src/main.js
+++ 02_Vue组件化/02_composition_api/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 // import App from './01_Ref和Reactive/App.vue'
 // import App from './02_computed/App.vue'
-import App from './03_setup中使用ref/App.vue'
+// import App from './03_setup中使用ref/App.vue'
+import App from './04_setup中使用watch/App.vue'
 
 createApp(App).mount('#app')
