3d389317e3b23951688dc19193dcb4fd451f8aa2
白晗 <baih_1989@163.com>
2022-11-29 17:59:49 +0800

    学习vuex

--- 02_Vue组件化/04_vuex/src/Home.vue
+++ 02_Vue组件化/04_vuex/src/Home.vue
@@ -2,16 +2,22 @@
   <div class="home">
     <h2>Home 当前计数：{{ $store.state.counter }}</h2>
     <button @click="increment">+1</button>
+    <h2>name - {{ name }}</h2>
+    <button @click="changeName">修改name</button>
   </div>
 </template>
 
 <script setup>
+  import { toRefs } from 'vue'
   import { useStore } from 'vuex'
 
   const store = useStore()
   function increment() {
     store.commit("increment")
   }
+
+  const { name } = toRefs(store.state)
+
 </script>
 
 <style>
--- 02_Vue组件化/04_vuex/src/store/index.js
+++ 02_Vue组件化/04_vuex/src/store/index.js
@@ -2,7 +2,8 @@ import { createStore } from 'vuex'
 
 const store = createStore({
   state: () => ({
-    counter: 66
+    counter: 66,
+    name: 'Lyci'
   }),
   mutations: {
     increment(state) {
