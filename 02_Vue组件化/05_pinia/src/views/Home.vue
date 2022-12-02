17549e6e96a9d1175410ac192c3b6d5cdad5d1e5
白晗 <baih_1989@163.com>
2022-12-02 11:47:08 +0800

    学习pinia

---
+++ 02_Vue组件化/05_pinia/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h2>当前计数：{{ counter.count }}</h2>
+  </div>
+</template>
+
+<script setup>
+  import useCounter from '@/stores/counter';
+
+  const counter = useCounter()
+
+</script>
+
+<style scoped>
+</style>
