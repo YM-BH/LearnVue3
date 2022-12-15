ae9e362500abe6477a48c72bfa7b6cfd33a3f9ee
白晗 <baih_1989@163.com>
2022-12-15 17:58:28 +0800

    城市页面

--- trip/src/App.vue
+++ trip/src/App.vue
@@ -2,11 +2,15 @@
 <template>
   <div class="app">
     <router-view></router-view>
-    <tabbar></tabbar>
+    <!-- 根据当前路由，隐藏 TabBar -->
+    <tabbar v-show="!route.meta.hideTabBar"></tabbar>
   </div>
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 </script>
 
--- trip/src/views/city/city.vue
+++ trip/src/views/city/city.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="city">
-    city
+    <van-search v-model="searchValue" placeholder="城市/区域" shape="round" show-action />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+
+const searchValue = ref('')
 
 </script>
 
 <style lang="less" scoped>
-
+.city {
+  // --van-search-left-icon-color: orange;
+}
 </style>
